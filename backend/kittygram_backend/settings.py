4b9b45b55f2b97e3dfa43d1a97e8849c046cee5b
EkaterinaKaiser <seminaekaterina.11.10.98@gmail.com>
2025-03-03 21:25:25 +0300

    fix web3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'kittygram2.hopto.org,85.143.145.4,127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
